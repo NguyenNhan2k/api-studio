8b45ebb3473fb7c4218933bd5a4224869bd49ad6
unknown <dlhung.hg@gmail.com>
2023-06-05 09:33:19 +0700

    chi tiet ao cuoi update

--- src/routes/aocuoi.route.js
+++ src/routes/aocuoi.route.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 const { AocuoiController } = require('../controllers');
 router.get('/', AocuoiController.index);
-router.get('/detail/:id', AocuoiController.index);
+router.get('/detail/:id', AocuoiController.detail);
 module.exports = router;
